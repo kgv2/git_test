6ad963185719db22dfd6f41cf2fee977ef81d12c
kgv2 <145078111+kgv2@users.noreply.github.com>
2025-06-27 19:31:28 -0400

    Add code from first JS lesson

---
+++ variables-and-operators/javascript.js
@@ -0,0 +1,26 @@
+//assign
+let firstname = "John";
+let surname = "Doe";
+
+console.log(firstname);
+console.log(surname);
+
+//reassign
+let age = 11;
+
+console.log(age);
+
+age = 54; //already defined
+
+console.log(age);
+
+//use of const
+const pi = 3.14; //remains constant and cannot be changed
+
+//pi = 10; //throws error as cannot be rewritten
+
+console.log(pi);
+
+
+//math
+console.log((3+2) - 76 * (1+1));
